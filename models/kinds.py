5527b09b389d367a176fb0dc243ac8cf72467fff
makera <makars999mail@gmail.com>
2022-09-23 11:45:55 +0900

    models

---
+++ models/kinds.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass, fields
+
+from models import BaseModel
+
+
+@dataclass
+class FoodKind(BaseModel):
+    icon: str
+    name: str
+
+
+if __name__ == '__main__':
+    food = FoodKind(icon='plate', name='Dinner', id=None)
+    food._create_table()
+    food.save()
+    print(food)
+    food.name = 'Launch'
+    food.save()
+    print(food)
+    foods = FoodKind.fetch_all()
+    print(foods)
